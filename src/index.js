db62da5dc0fa63dcb0d6f91531537b75f2cf5f24
Kritesh12-slack <148207048+Kritesh12-slack@users.noreply.github.com>
2024-02-25 03:39:29 +0000

    updated utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 // require('dotenv').config({path : "./env"})  //not working as it is not ES6
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config({
-    path : "./env"
+    path: "./env"
 })
 
 connectDB()
+    .then(() => {
+        app.on("Error", (error) => {
+            console.log(error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Failed !!!!!!!!!", err)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import express from "express";
 // const app = express()
